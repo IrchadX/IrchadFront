8336a23e13603ce52cb756cbc62a31ff89559af1
wissalcodes <lw_messikh@esi.dz>
2025-03-05 10:28:42 +0100

    configured fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,16 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        main: '#2B7A78',
-        black: '#17252A',
-        secondary: '#3AAFA9',
-        white: '#FCFFFE',
+        main: "#2B7A78",
+        black: "#17252A",
+        secondary: "#3AAFA9",
+        white: "#FCFFFE",
+      },
+      fontFamily: {
+        futura: "var(--font-futura)",
+        montserrat: "var(--font-montserrat)",
       },
     },
   },
   plugins: [],
-}
+};
