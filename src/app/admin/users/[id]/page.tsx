4d7e04343900e3c7db23c444531c5a97246b3008
Rania Gueddouche <gueddoucherania@gmail.com>
2025-03-15 02:58:10 +0100

    updated user-modification form

--- src/app/admin/users/[id]/page.tsx
+++ src/app/admin/users/[id]/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { useParams } from "next/navigation";
 import {UserModification} from "@/components/admin/users/user_modification";
+import Title from "@/components/shared/title";
+
 
 export default function UserModificationPage() {
   const { id: userId } = useParams();
@@ -8,5 +10,10 @@ export default function UserModificationPage() {
   if (!userId) {
     return <div>Invalid user ID</div>;
   }
-  return <UserModification userId={Array.isArray(userId) ? userId[0] : userId} />;
+  return(
+    <div className="container mx-auto py-5">
+      <Title text="Modifier un utilisateur" lineLength="100px" />
+      <UserModification userId={Array.isArray(userId) ? userId[0] : userId} />
+    </div>)
+   ;
 }
