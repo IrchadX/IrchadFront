6f68fb1db06cf6676ab6a598783cc2e4e67a4231
Rania Gueddouche <gueddoucherania@gmail.com>
2025-03-17 21:44:24 +0100

    sidebarLinks commercial

--- src/data/sidebarLinks.ts
+++ src/data/sidebarLinks.ts
@@ -1,6 +1,7 @@
 import { IconType } from "react-icons";
 import { FiUsers, FiSettings } from "react-icons/fi";
-import { MdDevices, MdOutlineDashboard } from "react-icons/md";
+import { MdDevices, MdOutlineDashboard, MdLocalOffer  } from "react-icons/md";
+import { FaMoneyBillTrendUp } from "react-icons/fa6";
 import { TbWorld } from "react-icons/tb";
 
 export interface SidebarLink {
@@ -36,3 +37,32 @@ export const adminSidebarLinks: SidebarLink[] = [
     Icon: FiSettings,
   },
 ];
+
+export const commercialSidebarLinks: SidebarLink[] = [
+  {
+    name: "Tableau de Bord",
+    href: "/commercial/dashboard",
+    Icon: MdOutlineDashboard,
+  },
+  {
+    name: "Clients",
+    href: "/commercial/clients",
+    Icon: FiUsers,
+  },
+  {
+    name: "Ventes",
+    href: "/commercial/sales",
+    Icon: FaMoneyBillTrendUp,
+  },
+  {
+    name: "Offres",
+    href: "/commercial/offers",
+    Icon: MdLocalOffer,
+  },
+  {
+    name: "Paramètres",
+    href: "/commercial/settings",
+    Icon: FiSettings,
+  },
+];
+
